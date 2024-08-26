a22855f552c0016c5b86f57d0b22dcca449843d1
ianupiitaa <ianyofes73@gmail.com>
2024-08-26 17:00:19 -0600

    creacion del desencriptado js

--- index.js
+++ index.js
@@ -22,3 +22,23 @@ function encriptar(){
         alert("ingresa un texto, por favor");
     }
 }
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let textoCifrado = texto
+                        .replace(/3rq/gi, "e")
+                        .replace(/1mk/gi, "i")
+                        .replace(/0lz/gi, "o")
+                        .replace(/5xc/gi, "u")
+                        .replace(/8ds/gi, "a");
+    
+    if (document.getElementById("texto").value.length !== 0 ){
+        document.getElementById("texto").value = textoCifrado;
+        document.getElementById("cuerpo__mensaje__encriptado__titulo").textContent = "Texto desencriptado";
+        document.getElementById("parrafo").textContent = "";
+        //document.getElementById("Imagen1").src = ""               en caso de imagen
+    }else{
+        imagen.src = ".img/Imagen1.png";
+        alert("ingresa un texto, por favor");
+    }
+}
