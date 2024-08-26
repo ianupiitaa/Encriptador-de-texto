64df039b8a7340bc62b1b4dd395df4458911dd08
ianupiitaa <ianyofes73@gmail.com>
2024-08-26 14:39:40 -0600

    nueva logica

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 function encriptar(){
     let texto = document.getElementById("texto").value;
     let parrafo = document.getElementById("parrafo");
-    let imagen = document.getElementById("Imagen1")
+    let imagen = document.getElementById("Imagen1");
 
     let tituloMensaje = Document.getElementById("cuerpo__mensaje__encriptado__titulo");
     let textoCifrado = texto.replace(/e/gi , "erasmo").replace(/i/gi ,"iman").replace(/o/gi,"hora").replace(/u/gi,"yuya").replace(/a/gi,"nano");
     
-    if (texto.length =! 0 ){
+    if (texto.length != 0 ){
         texto = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado";
         parrafo.textContent = "";
