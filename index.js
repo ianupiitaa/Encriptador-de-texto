c69a9738656be511c7ef9d75d8f9e0f9f60d2601
ianupiitaa <ianyofes73@gmail.com>
2024-08-26 15:24:30 -0600

    correccion del encriptador js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
 function encriptar(){
     let texto = document.getElementById("texto").value;
-    let parrafo = document.getElementById("parrafo");
-    let imagen = document.getElementById("Imagen1");
+    //let parrafo = document.getElementById("parrafo");
+    //let imagen = document.getElementById("Imagen1");
+    //let tituloMensaje = document.getElementById("cuerpo__mensaje__encriptado__titulo");
 
-    let tituloMensaje = Document.getElementById("cuerpo__mensaje__encriptado__titulo");
     let textoCifrado = texto
-                        .replaceall("e", "erasmo")
-                        .replaceall("i", "iman")
-                        .replaceall("o", "hora")
-                        .replaceall("u", "yuya")
-                        .replaceall("a", "nano");
+                        .replace(/e/gi, "3rq")
+                        .replace(/i/gi, "1mk")
+                        .replace(/o/gi, "0lz")
+                        .replace(/u/gi, "5xc")
+                        .replace(/a/gi, "8ds");
     
-    if (texto.length != 0 ){
-        texto = textoCifrado;
-        tituloMensaje.textContent = "Texto encriptado";
-        parrafo.textContent = "";
-        //imagen.src = ""               en caso de imagen
+    if (document.getElementById("texto").value.length !== 0 ){
+        document.getElementById("texto").value = textoCifrado;
+        document.getElementById("cuerpo__mensaje__encriptado__titulo").textContent = "Texto encriptado";
+        document.getElementById("parrafo").textContent = "";
+        //document.getElementById("Imagen1").src = ""               en caso de imagen
     }else{
         imagen.src = ".img/Imagen1.png";
         alert("ingresa un texto, por favor");
