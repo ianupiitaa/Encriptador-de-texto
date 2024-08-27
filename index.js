de4c7c1741409e4d40b0cf718a91d88f8c3dc90a
ianupiitaa <ianyofes73@gmail.com>
2024-08-27 00:20:15 -0600

    logica nueva trim()

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-function removefoto(){
+function removefoto() {
     let imagen = document.getElementById("Imagen1");
     imagen.style.display = 'none';
-}
-function encriptar(){
+  }
+  
+  function encriptar() {
+    let texto = document.getElementById("texto").value.trim(); 
+  
+    if (texto.length !== 0) {
+      let textoCifrado = texto
+        .replace(/e/gi, "3rq")
+        .replace(/i/gi, "1mk")
+        .replace(/o/gi, "0lz")
+        .replace(/u/gi, "5xc")
+        .replace(/a/gi, "8ds");
+  
+      document.getElementById("cuerpo__mensaje__encriptado__titulo").textContent = "Texto encriptado";
+      document.getElementById("parrafo").textContent = "";
+      removefoto();
+      let seccion = document.getElementById("seccion");
+      seccion.textContent = textoCifrado;
+    } else {
+      alert("Ingresa un texto, por favor");
+    }
+  }
+  
+  function desencriptar() {
     let texto = document.getElementById("texto").value.trim();
-
-    let textoCifrado = texto
-                        .replace(/e/gi, "3rq")
-                        .replace(/i/gi, "1mk")
-                        .replace(/o/gi, "0lz")
-                        .replace(/u/gi, "5xc")
-                        .replace(/a/gi, "8ds");
-    
-    if (document.getElementById("texto").value.length !== 0 ){
-
-        document.getElementById("cuerpo__mensaje__encriptado__titulo").textContent = "Texto encriptado";
-        document.getElementById("parrafo").textContent = "";
-        removefoto();
-        let seccion = document.getElementById("seccion");
-        seccion.textContent = textoCifrado;
-    }else{
-        imagen.src = ".img/Imagen1.png";
-        alert("ingresa un texto, por favor");
+  
+    if (texto.length !== 0) {
+      let textoDescifrado = texto
+        .replace(/3rq/gi, "e")
+        .replace(/1mk/gi, "i")
+        .replace(/0lz/gi, "o")
+        .replace(/5xc/gi, "u")
+        .replace(/8ds/gi, "a");
+  
+      document.getElementById("cuerpo__mensaje__encriptado__titulo").textContent = "Texto desencriptado";
+      document.getElementById("parrafo").textContent = "";
+      removefoto();
+      let seccion = document.getElementById("seccion");
+      seccion.textContent = textoDescifrado;
+    } else {
+      let imagen = document.getElementById("Imagen1");
+      imagen.style.display = 'block';
     }
-}
-
-function desencriptar(){
-    let texto = document.getElementById("texto").value;
-    let textoCifrado = texto
-                        .replace(/3rq/gi, "e")
-                        .replace(/1mk/gi, "i")
-                        .replace(/0lz/gi, "o")
-                        .replace(/5xc/gi, "u")
-                        .replace(/8ds/gi, "a");
-    
-    if (document.getElementById("texto").value.length !== 0 ){
-        
-        document.getElementById("cuerpo__mensaje__encriptado__titulo").textContent = "Texto desencriptado";
-        document.getElementById("parrafo").textContent = "";
-        removefoto();
-        let seccion = document.getElementById("seccion");
-        seccion.textContent = textoCifrado;
-        
-    }else{
-        let imagen = document.getElementById("Imagen1");
-        imagen.style.display = 'block';
-    }
-}
+  }
